bdc6ba2478143e46ff227ea376fb302d4fc87d65
yasuko <y.otsubo04@gmail.com>
2025-07-13 14:40:47 +0900

    style組み込み

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,64 @@
 "use client";
 
-//import Home from "./Home/index";
-//import Header from "./Header/index";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+import classes from "../styles/Home.module.scss";
+import { Post } from "@/types/post";
 
+export default function Home() {
+  const [posts, setPosts] = useState<Post[]>([]);
 
-import dynamic from 'next/dynamic';
+  useEffect(() => {
+    const fetcher = async () => {
+      const res = await fetch("api/posts");
+      const { posts } = await res.json();
+      setPosts(posts);
+    };
 
-const Header = dynamic(() => import('./_components/Header'), { ssr: false });
-const Home = dynamic(() => import('./Home'), { ssr: false });
+    fetcher();
+  }, []);
 
-export default function Page() {
   return (
-    <>
-      <Header />
-      <Home />
-    </>
+    <div className="">
+      <div className={classes.container}>
+        <ul>
+          {/* 記事の一覧をmap処理で繰り返し表示します。*/}
+          {posts.map((post) => {
+            return (
+              <li key={post.id} className={classes.list}>
+                <Link href={`/posts/${post.id}`} className={classes.link}>
+                  <div className={classes.post}>
+                    <div className={classes.postContent}>
+                      <div className={classes.postInfo}>
+                        <div className={classes.postDate}>
+                          {new Date(post.createdAt).toLocaleDateString()}
+                        </div>
+                        <div className={classes.postCategories}>
+                          {post.postCategories.map((pc) => {
+                            return (
+                              <div
+                                key={pc.category.id}
+                                className={classes.postCategory}
+                              >
+                                {pc.category.name}
+                              </div>
+                            );
+                          })}
+                        </div>
+                      </div>
+                      <p className={classes.postTitle}>{post.title}</p>
+                      <div
+                        className={classes.postBody}
+                        dangerouslySetInnerHTML={{ __html: post.content }}
+                      />
+                    </div>
+                  </div>
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
   );
 }
